1e59d4857a023b1d66ab57b22547af9b5bcc5178
dhananjaymudgule <mudguledhananjay@gmail.com>
2025-03-17 11:19:45 +0530

    code refactor 1

--- app/chat_ui/app.py
+++ app/chat_ui/app.py
@@ -10,9 +10,9 @@ load_dotenv()
 
 # FastAPI Backend URL
 # local 
-API_BASE_URL = "http://127.0.0.1:8000/"
+# API_BASE_URL = "http://127.0.0.1:8000/"
 # cloud
-# API_BASE_URL = os.getenv("API_BASE_URL")
+API_BASE_URL = os.getenv("API_BASE_URL")
 API_ENDPOINT = f"{API_BASE_URL}/chat/" 
 
 
